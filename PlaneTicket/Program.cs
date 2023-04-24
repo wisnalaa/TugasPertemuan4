75b8a5dd12a90fb4588b8bada05c7e2022f45fef
wisnalaa <wisnala34@gmail.com>
2023-04-25 06:38:37 +0700

    tugas tiket pesawat

---
+++ PlaneTicket/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaneTicket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TiketPesawat ticket1 = new TiketPesawat();
+            TiketPesawat ticket2 = new TiketPesawat();
+
+            ticket1.Origin = "JOGJA";
+            ticket1.Destination = "PKU";
+            ticket1.Cost = 100;
+            ticket1.Currency = "USD";
+
+            ticket2.Origin = "JKT";
+            ticket2.Destination = "PKU";
+            ticket2.Cost = 750000;
+            ticket2.Currency = "IDR";
+
+            ticket1.PrintSummary();
+            Console.WriteLine();
+            ticket2.PrintSummary();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PlaneTicket/TiketPesawat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaneTicket
+{
+    internal class TiketPesawat
+    {
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public int Cost { get; set; }
+        public string Currency { get; set; }
+
+        public void PrintSummary()
+        {
+            Console.WriteLine("Origin: {0}", Origin);
+            Console.WriteLine("Destination: {0}", Destination);
+            Console.WriteLine("Cost: {0} ({1})", Cost, Currency);
+
+            if (Currency == "USD")
+            {
+                Cost = Cost * 15000;
+                Console.WriteLine("Convert to Rupiah: {0}", Cost);
+            }
+            else if (Currency == "IDR")
+            {
+                Cost = Cost / 15000;
+                Console.WriteLine("Convert to Dolar: {0}", Cost);
+            }
+        }
+    }
+}
